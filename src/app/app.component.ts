1a4f2f6c2c5dc4aab1c6c627be306701761438e0
pyone <pyoneeisan@gmail.com>
2022-12-17 16:30:09 +0700

    updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { SuperHero } from './models/super-hero';
+import { SuperHeroService } from './services/super-hero.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'SuperHero.UI';
+  heroes:SuperHero[]=[];
+  heroToEdit?:SuperHero;
+
+  constructor(private superHeroService: SuperHeroService){};
+  
+  ngOnInit() {
+    
+    this.superHeroService
+    .getSuperHero()
+    .subscribe((result:SuperHero[]) => this.heroes=result);
+  }
+
+  updatedHeroList(heroes:SuperHero[])
+  {
+    this.heroes=heroes;
+  }
+
+  initNewHero(){
+    this.heroToEdit=new SuperHero();
+  }
+
+  editHero(hero:SuperHero){
+    this.heroToEdit=hero;
+  }
 }
---
+++ src/app/services/super-hero.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { SuperHero } from '../models/super-hero';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SuperHeroService {
+  private url="SuperHero"
+  constructor(private http:HttpClient) { }
+
+  public getSuperHero(): Observable<SuperHero[]> {
+    return this.http.get<SuperHero[]>(`${environment.apiurl}/${this.url}`);
+  }
+  public updateHero(hero:SuperHero): Observable<SuperHero[]> {
+    return this.http.put<SuperHero[]>(`${environment.apiurl}/${this.url}`,hero);
+  }
+  public createHero(hero:SuperHero): Observable<SuperHero[]> {
+    return this.http.post<SuperHero[]>(`${environment.apiurl}/${this.url}`,hero);
+  }
+  public deleteHero(hero:SuperHero): Observable<SuperHero[]> {
+    return this.http.delete<SuperHero[]>(`${environment.apiurl}/${this.url}/${hero.id}`);
+  }
+}
